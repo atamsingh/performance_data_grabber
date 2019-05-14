2c212bbfee7f05cc13ac11542834f0d3ddebe760
Areez Gangji <areez@shopify.com>
2019-05-14 11:28:19 -0400

    writehead incorrect, fixing

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var binPath = '/repo/node_modules/phantomjs/lib/phantom/bin/phantomjs'
 console.log(binPath)
 
 var server = http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.writeHead(200, {'Content-Type': 'application/json'});
   var q = url.parse(req.url, true).query;
   file_name = uuid() + ".json";
   var txt = binPath  + " " + "/repo/fetch_timings.js " + q.address + " " + file_name;
@@ -21,7 +21,6 @@ var server = http.createServer(function (req, res) {
     console.log("Deleting file...")
     fs.unlinkSync(file_name);
     console.log("File deleted");
-    res.writeHead(200, {"Content-Type": "application.json"});
     res.end(contents);
   })
 }).listen(80);
